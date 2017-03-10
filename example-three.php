ae038f105f04c0e4acdce1176034bd7eeaf88ba9
Matt Windle <matthewwindle@outlook.com>
2017-03-10 23:41:20 +0000

    Added example three to website

--- example-three.php
+++ example-three.php
@@ -13,19 +13,13 @@
   <div class="overlay" >
     <img class="close-btn" src="/images/error.svg" width="50px">
     <pre class="prettyprint shadow">
-      .blog-grid{
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: auto;
-        grid-auto-flow: row;
-      }
     </pre>
   </div>
    <h1 class="title" >CSS Grids</h1>
   <div class="main-container" >
     <main>
       <div class="grid example-one shadow" >
-        <header><h1>Blog Post Layout <span>- Grid Auto Flow</span></h1></header>
+        <header><h1>Blog Post Layout <span>- Grid Template Area</span></h1></header>
         <div class="side-left" >
           <nav>
             <a href="/index.php" >What are CSS Grids</a>
@@ -36,8 +30,21 @@
           </nav>
         </div>
         <section>
-
+          <p class="content-block content">
+            Within CSS Grids there is a property that can be used on the grid childs called <strong>grid-area</strong>. This allows the child elements to be given a name (grid-area) simular to an ID that can be called upon in the grid to position the child accordingly.
+          </p>
+          <p class="content-block content">
+            The Grid below has been coded using the grid-template-area and grid-area properties.
+          </p>
+          <div class="grid-template-example" >
+            <div class="grid-template-header"></div>
+            <div class="grid-template-leftside"></div>
+            <div class="grid-template-main"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+            <div class="grid-template-rightside"></div>
+            <div class="grid-template-footer"></div>
+          </div>
         </section>
+
         <footer><p>Example Three - CSS Grid - Matthew Windle</p></footer>
       </div> <!-- end of example one -->
       <div class="button-wrapper" >
